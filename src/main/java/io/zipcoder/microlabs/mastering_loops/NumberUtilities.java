9ef0171c76444cabae777b9715c0c96281c48a67
Kristofer <kris@youngers.org>
2024-10-28 14:41:53 -0400

    Initial commit

---
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -0,0 +1,41 @@
+package io.zipcoder.microlabs.mastering_loops;
+
+
+public class NumberUtilities {
+
+    public static String getExponentiations(int start, int stop, int step, int exponent) {
+        return null;
+    }
+
+    public static String getRange(int start, int stop, int step) {
+        return null;
+    }
+
+    public static String getRange(int start, int stop) {
+        return null;
+    }
+
+    public static String getRange(int stop) {
+        return null;
+    }
+
+    /*
+     * natural break
+     */
+
+    public static boolean isNumberEven(int toTest) { return false; }
+    public static boolean isNumberOdd(int toTest) { return false; }
+
+    public static String getEvenNumbers(int start, int stop) {
+        return null;
+    }
+
+    public static String getOddNumbers(int start, int stop) {
+        return null;
+    }
+
+    public static String getSquareNumbers(int start, int stop, int step) {
+        return null;
+    }
+
+}
---
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -0,0 +1,22 @@
+package io.zipcoder.microlabs.mastering_loops;
+
+public class TriangleUtilities {
+
+    public static String getRow(int numberOfStars) {
+        return null;
+    }
+
+    public static String getTriangle(int numberOfRows) {
+        return null;
+    }
+
+    // hmm
+    
+    public static String getSmallTriangle() {
+        return null;
+    }
+
+    public static String getLargeTriangle() {
+        return null;
+    }
+}
